c37ba300cfa170bae00fdb77b0001bfae41b8016
sage7978 <44961824+sage7978@users.noreply.github.com>
2021-07-26 15:32:01 +0530

    Update urls.py

--- WeConnect/urls.py
+++ WeConnect/urls.py
@@ -19,6 +19,7 @@ from django.urls import path,include
 from django.conf.urls import url
 from django.conf import settings
 from django.views.static import serve
+from django.conf.urls.defaults import *
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -27,4 +28,5 @@ urlpatterns = [
     path('oauth/',include('social_django.urls',namespace='social')),
     url(r'^media/(?P<path>.*)$', serve,{'document_root':       settings.MEDIA_ROOT}), 
     url(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}), 
+    (r'^favicon\.ico$', 'django.views.generic.simple.redirect_to', {'url': '/static/images/favicon.ico'}),
 ]
