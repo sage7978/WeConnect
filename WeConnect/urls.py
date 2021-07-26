a7556687cb88da3ec87320611cd0b38e888aefe9
sage7978 <44961824+sage7978@users.noreply.github.com>
2021-07-26 14:40:02 +0530

    Update urls.py

--- WeConnect/urls.py
+++ WeConnect/urls.py
@@ -16,6 +16,9 @@ Including another URLconf
  # path('register/',include('req.urls')),
 from django.contrib import admin
 from django.urls import path,include
+from django.conf.urls import url
+from django.conf import settings
+from django.views.static import serve
 
 urlpatterns = [
     path('admin/', admin.site.urls),
