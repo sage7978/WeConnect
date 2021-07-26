b5798726b0e5c52aa6cf1835f2efc462e8fcb095
sage7978 <44961824+sage7978@users.noreply.github.com>
2021-07-26 14:45:06 +0530

    Update settings.py

--- WeConnect/settings.py
+++ WeConnect/settings.py
@@ -129,6 +129,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+PROJECT_DIR = os.path.dirname(os.path.abspath(__file__))
+STATIC_ROOT = os.path.join(PROJECT_DIR, 'static')
 
 LOGIN_REDIRECT_URL = 'dashboard'
 LOGIN_URL = 'login'
